1e457e0087f806d485df7d787b5226431984e902
UT02004 <UT02004@UNICOMTIC74>
2024-10-11 16:26:12 +0530

    I completed 10th and 11th Questions

---
+++ BookRentalManagementSystem_V2/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V2
+{
+    internal class Book
+    {
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal RentalPrice { get; set; }
+
+
+        public static int TotalBooks = 0;
+
+
+        public Book(string bookId, string title, string author, decimal rentalPrice)
+        {
+            BookId = bookId;
+            Title = title;
+            Author = author;
+            RentalPrice = rentalPrice;
+            TotalBooks++;
+        }
+
+       
+    }
+}
